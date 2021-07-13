182a5300c977458e6cfa8f7fea8ac137ce14f793
ctscofield <ctscofield@gmail.com>
2021-07-13 14:43:18 -0600

    set up and make-cars-table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema
+    .createTable('cars', table => {
+      table.increments('id')
+      table.text('vin', 17)
+        .unique()
+        .notNullable()
+      table.text('make')
+        .notNullable()
+      table.text('model')
+        .notNullable()
+      table.number('mileage')
+        .notNullable()
+      table.text('title')
+      table.table('transmission')
+    })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema
+    .dropTableIfExists('cars')
 };
